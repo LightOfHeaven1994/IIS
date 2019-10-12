3e9f0ade72bec01151221692bf0a1b95444648c0
eshamard <eshamard@redhat.com>
2019-10-12 23:02:52 +0200

    Add navigation bar

--- cinema.py
+++ cinema.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -11,6 +11,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='about')
+
+
+@app.route('/program')
+def program():
+    return render_template('program.html', title='program')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='login')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='register')
 
 
 if __name__ == '__main__':
