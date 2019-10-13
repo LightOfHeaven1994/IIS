bc943afc2fca42380449a62363ddc2143f3f2f56
eshamard <eshamard@redhat.com>
2019-10-13 11:13:22 +0200

    Add Register and LogIn pages, fix links

--- cinema.py
+++ cinema.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7749a40aa6e1e5d1b179d879aef51844'
 
 
 @app.route('/')
@@ -17,14 +21,26 @@ def about():
 def program():
     return render_template('program.html', title='program')
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='login')
+	form = LoginForm()
+	if form.validate_on_submit(): # check if filled data is valid
+		if form.email.data == 'admin@cinema.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', title='login', form=form)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='register')
+	form = RegistrationForm()
+	if form.validate_on_submit(): # check if filled data is valid
+		flash(f'{form.username.data}, Thanks for registering!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='register', form=form)
 
 
 if __name__ == '__main__':
