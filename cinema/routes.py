3ca2d89653d9e2712c36cd9b2a49618bd41c540f
eshamard <eshamard@redhat.com>
2019-11-22 23:39:37 +0100

    Add database for Events, add dynamic tabs, add dropdown for admin

--- cinema/routes.py
+++ cinema/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from cinema import app, db, bcrypt
 from cinema.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from cinema.models import User
+from cinema.models import User, Event
 from flask_login import login_user, current_user, logout_user, login_required
 
 
