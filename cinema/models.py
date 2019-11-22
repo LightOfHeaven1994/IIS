af340bee16282a8d29e6a3e77fd47f70ca5f336b
eshamard <eshamard@redhat.com>
2019-11-22 18:46:46 +0100

    Add: update profile(username, email, picture), fix gitignore

--- cinema/models.py
+++ cinema/models.py
@@ -12,6 +12,7 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(60), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
+	profile_picture = db.Column(db.String(20), nullable=False, default='default.jpg')
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}')"
